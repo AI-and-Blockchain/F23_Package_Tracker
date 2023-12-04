0d084402e770550f7124a14f9c6241053bb4a647
Aneeshsaikolukuluri <kolukulurianeesh@gmail.com>
2023-12-03 19:23:10 -0500

    QR works with mobile on any WiFi

--- QR1.py
+++ QR1.py
@@ -8,30 +8,48 @@ Created on Fri Nov 17 03:34:30 2023
 import qrcode
 import subprocess
 import webbrowser
+import socket
+
+
+def get_local_ip():
+    # Get the local IP address of the machine
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))  # Connect to a known external server
+        local_ip = s.getsockname()[0]
+        s.close()
+        return local_ip
+    except Exception as e:
+        print(f"Error getting local IP: {e}")
+        return None
+
 
 if __name__ == "__main__":
+    # Fetch the local IP address
+    local_ip = get_local_ip()
 
+    if local_ip:
+        # Construct the URL using the local IP address
+        port = 3000  # Replace with the actual port your server is running on
+        url = f"http://{local_ip}:{port}"
 
-    # Specify the path to the Python script you want to execute
-    url = "http://192.168.1.52:3000" 
-    
-    # Generate the QR code with the url
-    qr_code_data = url
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
-    
-    # Create an image from the QR code data
-    img = qr.make_image(fill_color="black", back_color="white")
-    
-    # Save the image to a file or display it
-    img.show()
-    webbrowser.open(url)
+        # Generate the QR code with the URL
+        qr_code_data = url
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(qr_code_data)
+        
+        # Create an image from the QR code data
+        img = qr.make_image(fill_color="black", back_color="white")
+        
+        # Save the image to a file or display it
+        img.show()
+    else:
+        print("Unable to fetch local IP address.")
 
 
     
